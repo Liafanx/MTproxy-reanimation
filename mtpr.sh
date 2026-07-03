52fc6da29a0d2cb64b946904072b0e66b25d9a62
LiafanX <liafanx@gmail.com>
2026-07-03 15:50:02 +0300

    Update mtpr.sh

--- mtpr.sh
+++ mtpr.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 # ═══════════════════════════════════════════════════════════════
-#  MTproxy-reanimation v1.1.2
+#  MTproxy-reanimation v1.1.3
 #  Telemt inbound SYN limiter + tuning manager
 #  https://github.com/Liafanx/MTproxy-reanimation
 # ═══════════════════════════════════════════════════════════════
 set -eo pipefail
 
-VERSION="1.1.2"
+VERSION="1.1.3"
 GITHUB_RAW="https://raw.githubusercontent.com/Liafanx/MTproxy-reanimation/main"
 INSTALL_DIR="/opt/mtproxy-reanimation"
 SETTINGS_FILE="${INSTALL_DIR}/settings.conf"
